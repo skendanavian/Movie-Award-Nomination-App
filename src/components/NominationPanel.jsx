303f8ed4f0b7890de3adb63f80848c933c44636b
skendanavian <s.nissen.bass@gmail.com>
2021-01-03 14:41:16 -0600

    changed text align for li items. Still fixing btn bug

--- src/components/NominationPanel.jsx
+++ src/components/NominationPanel.jsx
@@ -19,7 +19,13 @@ function NominationPanel({ nominees, nomineeTotal, removeNominee }) {
   return (
     <div className="nominee-panel">
       <div className="count-box">
-        {nomineeTotal <= 5 && (
+        {nomineeTotal === 0 && (
+          <>
+            <h3>Nominations</h3>
+            <p className="counter-text">(5 movies)</p>
+          </>
+        )}
+        {nomineeTotal > 0 && (
           <>
             <h3>Nominations</h3>
             <p className="counter-text">({nomineeTotal} / 5)</p>
